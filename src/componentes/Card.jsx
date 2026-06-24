14e14283cdc6af6163396baa5737dddd2c8c7ac5
22belen <belen.fleitas@gmail.com>
2026-06-24 19:30:14 -0300

    Card

---
+++ src/componentes/Card.jsx
@@ -0,0 +1,22 @@
+function Card({ producto }) {
+  return (
+    <div className="col-md-4 mb-4">
+      <div className="card h-100">
+        <img
+          src={producto.imagen}
+          className="card-img-top"
+          alt={producto.nombre}
+          style={{ height: "300px", objectFit: "cover" }}
+        />
+        <div className="card-body d-flex flex-column text-center">
+          <h5 className="card-title">{producto.nombre}</h5>
+          <p className="card-text">{producto.descripcion}</p>
+          <p className="card-text fw-bold">${producto.precio}</p>
+          <button className="btn btn-warning mt-auto">Pedir ahora</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
